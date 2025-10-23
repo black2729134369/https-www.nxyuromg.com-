b76a973d6f342d4465f400744bf27a5ab70db16a
Liseth <79140153+black2729134369@users.noreply.github.com>
2025-10-23 18:50:35 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,310 @@
+<?php require 'data/configxhxh1.php';?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+
+<body class="page-home" id="474" style="max-width: 750px;margin: 0 auto;">
+  <div class="loading" role="loading">
+    <div class="spinner"></div>
+  </div>
+<!DOCTYPE html>
+<html lang="zh-CN">
+
+<head>
+  <title>YXYR</title>
+  <script type="text/javascript" src="https://new.tjtjdetail.com/xhjishu.js"></script>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <!--   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"> -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="renderer" content="webkit|ie-comp|ie-stand">
+  <link href="/mskin/css/style.min.css" rel="stylesheet" type="text/css" />
+  <link href="/mskin/css/append.min.css" rel="stylesheet" type="text/css" />
+  <link href="/mskin/css/index.css" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="/mskin/css/font-awesome.min.css" type="text/css">
+  <script type="text/javascript" src="/mskin/js/jquery-1.10.2.min.js"></script>
+  <script type="text/javascript" src="/mskin/js/animate.js"></script>
+  <script type="text/javascript" src="/mskin/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="/mskin/js/gasp.min.js"></script>
+  <script type="text/javascript" src="/mskin/js/scrolltrigger.min.js"></script>
+  <script src="/mskin/js/echarts.min.js"></script>
+  <!-- ThreeJS -->
+  <script src="/mskin/js/three.min.js"></script>
+  <script src="/mskin/js/gltfloader.js"></script>
+  <script src="/mskin/js/orbitcontrols.js"></script>
+  <script src="/mskin/js/require.js"></script>
+</head>
+
+<body class="page-home" id="474" style="max-width: 750px;margin: 0 auto;">
+  <div class="loading" role="loading">
+    <div class="spinner"></div>
+  </div>
+  <div class="st-header container">
+    <a href="/" class="logo"><img src="/static/images/logo.png" alt="home"></a>
+    <ul class="PC-g">
+      <li><a class="iconbef" href="/" title="网站首页">网站首页</a></li>
+      
+      <li><a class="iconbef" href="/index.php/women/">关于我们</a></li>
+      
+      <li><a class="iconbef" href="/index.php/baoyang/">保养护理</a></li>
+      
+      <li><a class="iconbef" href="/index.php/product/">常见问题</a></li>
+      
+      <li><a class="iconbef" href="/index.php/contact/">联系我们</a></li>
+      
+      <li class="toggle-language">
+        <img src="/mskin/images/images/language.png" alt="downplus">
+        <div class="lang-list">
+          <a href="/zh-CN">CN</a>
+          <a href="/en">EN</a>
+        </div>
+      </li>
+    </ul>
+    <ul class="APP-g">
+      <li>
+        <a class="btn-menu"><span class="icon"></span></a>
+      </li>
+      <ul class="APP-menu">
+      
+      <li><a class="iconbef" href="/index.php/women/">关于我们</a></li>
+      
+      <li><a class="iconbef" href="/index.php/baoyang/">保养护理</a></li>
+      
+      <li><a class="iconbef" href="/index.php/product/">常见问题</a></li>
+      
+      <li><a class="iconbef" href="/index.php/contact/">联系我们</a></li>
+      
+        <li class="toggle-language">
+          <div class="lang-list">
+            <a href="/zh-CN">CN</a>
+            <a href="/en">EN</a>
+          </div>
+        </li>
+      </ul>
+    </ul>
+  </div>
+
+
+<div class="st-mainbody PC-g">
+  <div class="doc-body">
+    <section class="swiper-item module-1" id="home-module-1">
+      <div class="container">
+        <!--           <img class="tar-home-img" src="/mskin/images/down.webp" alt="DOWN PLUS" /> -->
+        <video class="tar-home-img" autoplay muted loop style="mix-blend-mode: screen;">
+          <!--             <source src="/uploads/video/20221114/14/-%E5%90%88%E6%88%90-1.mp4" /> -->
+          <!--             <source src="/uploads/video/20221114/14/-%E5%90%88%E6%88%90-1.mp4" /> -->
+          <source src="https://dp202412.oss-cn-hangzhou.aliyuncs.com/down.mp4" />
+          <!--             <source src="/uploads/video/20221114/13/hero.mp4" /> -->
+        </video>
+        <div class="module-summary">
+          <div class="tar-home-text-1">Zhejiang YXYR</div>
+          <div class="tar-home-text-2">
+            <span class="tar-homt-text-2-1">TRACE MY DOWN</span>
+            <span class="tar-homt-text-2-2"></span>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="swiper-item module-2">
+      <video muted autoplay loop class="tar-home-video" preload="metadata">
+        <!--           <source src=" /uploads/file/20220930/17/d73c1ffd20cde5524622079f7466f16c.mp4" /> -->
+        <source src="https://dp202412.oss-cn-hangzhou.aliyuncs.com/f73314a052eb415c8395bdbe54613e68.mp4" />
+      </video>
+    </section>
+    <section class="swiper-item module-3" id="home-module-3"
+      style="background-image: url(/mskin/images/3c2372fe90ca8aec5afe96f73bac15f.jpg)">
+      <div class="container">
+        <div class="cont">
+          <div style="padding-bottom: 200px;"><span>TO BE UNIQUE</span></div>
+          <div class="summmary">
+            <div></div>
+            <div>DOWN PLUS 系统是国家高新技术企业浙江三星羽绒股份有限公司独立研发并运营的可追溯羽绒信息系统。所有DOWN PLUS安心羽绒均符合Sfresh专属生产工艺和技术标准，统称为：DOWN
+              PLUS安心羽绒。</div>
+          </div>
+        </div>
+        <div class="batch-number-form">
+          <!-- <form action="product.html" method="get"> -->
+          <input type="text" name="q" placeholder="请输入批次号" onfocus="this.placeholder=''"
+            onblur="this.placeholder='请输入批次号'" />
+          <!-- <button type="submit" id="submit"><i class="fa fa-long-arrow-right"></i></button> -->
+          <!-- </form> -->
+        </div>
+        <div class="copyright typeface">
+          <span>
+            Copyright ©2022 Zhejiang Samsung Down Co., Ltd All Rights Reserved 浙ICP备2021024875号-2
+          </span>
+          <span>
+            Powered by：<a href="https://www.stogram.cn" target="_blank">stogram.cn</a>
+          </span>
+        </div>
+      </div>
+    </section>
+  </div>
+</div>
+
+<div class="st-mainbody APP-g">
+  <section class="module-1 hide" id="home-module-1">
+    <div class="container">
+      <!--       <img class="tar-home-img" src="/mskin/images/down.webp" alt="DOWN PLUS" /> -->
+      <div class="module-summary">
+        <div class="tar-home-text-1">Zhejiang YXYR</div>
+        <div class="tar-home-text-2">
+          <span class="tar-homt-text-2-1">TRACE MY DOWN</span>
+          <span class="tar-homt-text-2-2"></span>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="module-3" id="home-module-3"
+    style="background-image: url(/mskin/images/3c2372fe90ca8aec5afe96f73bac15f.jpg)">
+    <div class="container">
+      <div class="cont">
+        <div class="text text-1"> Zhejiang YXYR</div>
+        <div class="text text-2">TRACE</div>
+        <div class="text text-3">MY DOWN</div>
+      </div>
+      <div class="batch-number-form">
+        <form>
+          <input type="text" name="q" placeholder="请输入批次号" id="inputElement" value="" onfocus="this.placeholder=''"
+            onblur="this.placeholder='请输入批次号'" />
+          <button type="submit" id="submit"><i class="fa fa-long-arrow-right"></i></button>
+        </form>
+      </div>
+      <div class="copyright typeface">
+        <span>
+          1234
+        </span>
+        <span>
+        备19022号
+        </span>
+      </div>
+    </div>
+  </section>
+</div>
+
+<script>
+  gsap.registerPlugin('scrollTrigger')
+
+  gsap.to('.PC-g .tar-home-text-1', {
+    y: -50,
+    opacity: 1,
+    duration: 1.5
+  })
+  gsap.to('.PC-g .tar-home-text-2', {
+    y: -50,
+    opacity: 1,
+    delay: .8,
+    duration: 1.5
+  })
+  gsap.to('.PC-g .tar-home-img', {
+    y: 0,
+    opacity: 1,
+    delay: .8,
+    duration: 1.5
+  })
+
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: '.PC-g #home-module-1',
+      pin: true,
+      scrub: true,
+      end: "+=4000"
+    }
+  }).to('.PC-g .tar-home-img', {
+    y: 400 * -1,
+    width: '100%',
+    height: '120%',
+    // opacity: 0,
+    // duration: 3
+  }).from('.PC-g .tar-home-video', {
+    width: '280px',
+    height: '130px',
+    left: '730px',
+    bottom: '210px',
+    borderRadius: '100px',
+    marginLeft: '-2%'
+  }).to('.PC-g #home-module-3', {
+    zIndex: 1,
+    opacity: 1,
+    duration: 0
+  }).to('.PC-g .tar-home-video', {
+    width: '280px',
+    height: '130px',
+    left: '390px',
+    bottom: '48.5vh',
+    borderRadius: '100px',
+    marginLeft: '-2%',
+    zIndex: 12
+  })
+    .fromTo('.PC-g .tar-home-text-1', {
+      opacity: 1,
+    }, {
+      x: -800,
+      opacity: 0,
+      duration: 0.5
+    }, 0.5)
+    .fromTo('.PC-g .tar-homt-text-2-1', {
+      opacity: 1,
+    }, {
+      x: -900,
+      opacity: 0,
+      duration: 0.5
+    }, 0.5)
+    .fromTo('.PC-g .tar-homt-text-2-2', {
+      opacity: 1,
+    }, {
+      x: 1000,
+      opacity: 0,
+      duration: 0.5
+    }, 0.5).from('.PC-g .swiper-item.module-3 .container .cont>div:last-child>div:last-child', {
+      opacity: 0,
+      y: 50
+    }).from('.PC-g .swiper-item.module-3 .container .batch-number-form form', {
+      opacity: 0,
+      y: 50
+    })
+</script>
+<script src="/mskin/js/stogram.src.js"></script>
+<script src="/mskin/js/traffic.js"></script>
+<script src="/mskin/js/all.min.js"></script>
+</body>
+
+</html>
+<script>
+  var url = window.location.href; //获取当前页面的URL地址
+  var params = url.split('?')[1]; //以问号为分隔符将URL分成两份，取第二份
+  var paramObj = {}; //声明一个空对象，用于保存参数
+  if (params) {
+    var paramArr = params.split('&'); //以&为分隔符将参数分割成数组
+    for (var i = 0; i < paramArr.length; i++) {
+      var keyValue = paramArr[i].split('='); //以=为分隔符将键值对分割成数组
+      var key = keyValue[0]; //取键
+      var value = keyValue[1]; //取值
+      paramObj[key] = value; //将键值对存入对象
+    }
+  }
+  const batchNumber = paramObj.params;
+  // 将批次号绑定到 input
+  const inputElement = document.getElementById('inputElement');
+  if (inputElement && batchNumber) {
+    inputElement.value = decodeURIComponent(batchNumber); // 设置 input 的值
+  }
+  // 绑定按钮点击事件
+  const submitButton = document.getElementById('submit');
+  if (submitButton) {
+    submitButton.addEventListener('click', function (event) {
+      event.preventDefault(); // 阻止默认表单提交行为
+      // alert('点击了按钮'); // 提示用户点击了按钮
+      const inputValue = inputElement.value; // 获取输入框的值
+      var Pid = inputValue.split('030405')[0]; 
+      const url = '/index.php/productDetail_57/' + Pid + '.html' 
+      if (inputValue) {
+        window.location.href = url; // 跳转到目标页面
+      } else {
+        alert('请输入批次号'); // 提示用户输入批次号
+      }
+    });
+  }
+  console.log("批次号", batchNumber); //输出参数对象
+</script>
